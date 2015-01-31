f23eed6ebc026f445676e1402bd4a83ca1c4f100
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-01-31 02:01:40 +0100

    Added support for large rooms with dimensions higher than 1

--- src/Map.cpp
+++ src/Map.cpp
@@ -28,7 +28,7 @@ void Map::Load(void)
 	// Hard-coded
 	levels.push_back(new Level(10,10,1,1));
 	levels.push_back(new Level(11,10,1,1));
-	levels.push_back(new Level(11,11,1,1));
+	levels.push_back(new Level(10,11,4,5));
 
 	connections.push_back(new LevelConnection(levels[1],levels[0]));
 	connections.push_back(new LevelConnection(levels[1],levels[2]));
@@ -38,19 +38,74 @@ void Map::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
 	sf::Sprite smallRoom(Resources::texMap, sf::IntRect(0,0,BLOCK_SIZE,BLOCK_SIZE));
 	
+	sf::Sprite tlCorner(Resources::texMap, sf::IntRect(BLOCK_SIZE,0,BLOCK_SIZE,BLOCK_SIZE));              // Top-left
+	sf::Sprite trCorner(Resources::texMap, sf::IntRect(BLOCK_SIZE*3,0,BLOCK_SIZE,BLOCK_SIZE));            // Top-right
+	sf::Sprite blCorner(Resources::texMap, sf::IntRect(BLOCK_SIZE,BLOCK_SIZE*2,BLOCK_SIZE,BLOCK_SIZE));   // Bottom-left
+	sf::Sprite brCorner(Resources::texMap, sf::IntRect(BLOCK_SIZE*3,BLOCK_SIZE*2,BLOCK_SIZE,BLOCK_SIZE)); // Bottom-right
+
+	sf::Sprite tEdge(Resources::texMap, sf::IntRect(BLOCK_SIZE*2,0,BLOCK_SIZE,BLOCK_SIZE));            // Top
+	sf::Sprite rEdge(Resources::texMap, sf::IntRect(BLOCK_SIZE*3,BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE));   // Right
+	sf::Sprite bEdge(Resources::texMap, sf::IntRect(BLOCK_SIZE*2,BLOCK_SIZE*2,BLOCK_SIZE,BLOCK_SIZE)); // Bottom
+	sf::Sprite lEdge(Resources::texMap, sf::IntRect(BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE));     // Left
+	
+	sf::Sprite center(Resources::texMap, sf::IntRect(BLOCK_SIZE*2,BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE)); // Center
+
 	for(unsigned int i = 0; i < levels.size(); ++i)
 	{
 		Level *level = levels[i];
+		unsigned int x = level->GetX();
+		unsigned int y = level->GetY();
 		unsigned int w = level->GetWidth();
 		unsigned int h = level->GetHeight();
 
 		if(w == 1 && h == 1) // Small room
 		{
-			smallRoom.setPosition(level->GetX() * BLOCK_SIZE, level->GetY() * BLOCK_SIZE);
+			smallRoom.setPosition(x * BLOCK_SIZE, y * BLOCK_SIZE);
 			target.draw(smallRoom, states);
+		}
+		else if(w == 1) // Vertical room
+		{
+		}
+		else if(h == 1) // Horizontal room
+		{
 		}
 		else
 		{
+			// Set corners
+			tlCorner.setPosition(x * BLOCK_SIZE, y * BLOCK_SIZE);
+			trCorner.setPosition((x + w - 1) * BLOCK_SIZE, y * BLOCK_SIZE);
+			blCorner.setPosition(x * BLOCK_SIZE, (y + h - 1) * BLOCK_SIZE);
+			brCorner.setPosition((x + w - 1) * BLOCK_SIZE, (y + h - 1) * BLOCK_SIZE);
+			target.draw(tlCorner, states);
+			target.draw(trCorner, states);
+			target.draw(blCorner, states);
+			target.draw(brCorner, states);
+
+			// Top & Bottom lines
+			for(unsigned int i = 1; i < w - 1; ++i)
+			{
+				tEdge.setPosition((x + i) * BLOCK_SIZE, y * BLOCK_SIZE);
+				bEdge.setPosition((x + i) * BLOCK_SIZE, (y + h - 1) * BLOCK_SIZE);
+				target.draw(tEdge, states);
+				target.draw(bEdge, states);
+			}
+
+			// Left & Right lines
+			for(unsigned int i = 1; i < h - 1; ++i)
+			{
+				lEdge.setPosition(x * BLOCK_SIZE, (y + i) * BLOCK_SIZE);
+				rEdge.setPosition((x + w - 1) * BLOCK_SIZE, (y + i) * BLOCK_SIZE);
+				target.draw(lEdge, states);
+				target.draw(rEdge, states);
+			}
+
+			// Center
+			for(unsigned int i = 1; i < w - 1; ++i)
+				for(unsigned int j = 1; j < h - 1; ++j)
+				{
+					center.setPosition((x + i) * BLOCK_SIZE, (y + j) * BLOCK_SIZE);
+					target.draw(center, states);
+				}
 		}
 	}
 
