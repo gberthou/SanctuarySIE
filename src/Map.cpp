aa0fc741bdb8d5debed221589317e6ae6f46a820
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-01-31 01:04:26 +0100

    Door implementation from left to right

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+
 #include "Resources.h"
 #include "Map.h"
 
 const unsigned int BLOCK_SIZE = 32;
+
+const unsigned int HDOOR_WIDTH = BLOCK_SIZE;
+const unsigned int HDOOR_HEIGHT = 2;
+
+const unsigned int VDOOR_WIDTH = 2;
+const unsigned int VDOOR_HEIGHT = BLOCK_SIZE;
 
 Map::Map()
 {
@@ -21,11 +29,14 @@ void Map::Load(void)
 	levels.push_back(new Level(10,10,1,1));
 	levels.push_back(new Level(11,10,1,1));
 	levels.push_back(new Level(11,11,1,1));
+
+	connections.push_back(new LevelConnection(levels[0],levels[1]));
 }
 
 void Map::draw(sf::RenderTarget &target, sf::RenderStates states) const
 {
 	sf::Sprite smallRoom(Resources::texMap, sf::IntRect(0,0,BLOCK_SIZE,BLOCK_SIZE));
+	
 	for(unsigned int i = 0; i < levels.size(); ++i)
 	{
 		Level *level = levels[i];
@@ -35,11 +46,71 @@ void Map::draw(sf::RenderTarget &target, sf::RenderStates states) const
 		if(w == 1 && h == 1) // Small room
 		{
 			smallRoom.setPosition(level->GetX() * BLOCK_SIZE, level->GetY() * BLOCK_SIZE);
-			target.draw(smallRoom);
+			target.draw(smallRoom, states);
 		}
 		else
 		{
 		}
+	}
+
+	drawDoors(target, states);
+}
+
+void Map::drawDoors(sf::RenderTarget &target, sf::RenderStates states) const
+{
+	// The following constants define the 4 neighbours (left, up, down, right)
+	const int DX[4] = {-1, 0, 0, 1};
+	const int DY[4] = {0, -1, 1, 0};
+
+	sf::Sprite verticalDoor(Resources::texMap, sf::IntRect(0,BLOCK_SIZE,VDOOR_WIDTH,VDOOR_HEIGHT));
+	sf::Sprite horizontalDoor(Resources::texMap, sf::IntRect(0,BLOCK_SIZE+VDOOR_HEIGHT-VDOOR_HEIGHT,HDOOR_WIDTH,HDOOR_HEIGHT));
+
+	for(unsigned int i = 0; i < connections.size(); ++i)
+	{
+		bool done = false;
+		Level *level1 = connections[i]->level1;
+		Level *level2 = connections[i]->level2;
+
+		// For each block unit in level1
+		for(unsigned int x = 0; x < level1->GetWidth() && !done; ++x)
+			for(unsigned int y = 0; y < level1->GetHeight() && !done; ++y)
+			{
+				// Test all neighbours
+				for(unsigned int j = 0; j < 4; ++j) 
+				{
+					int tmpX = level1->GetX() + x + DX[j];
+					int tmpY = level1->GetY() + y + DY[j];
+
+					int x2 = level2->GetX();
+					int y2 = level2->GetY();
+
+					// If the neighbour is inside the second level
+					if(tmpX >= x2 && tmpX < x2 + (int) level2->GetWidth()
+				        && tmpY >= y2 && tmpY < y2 + (int) level2->GetHeight())
+					{
+						// Test the side of the connection
+						switch(j)
+						{
+							case 1: // Up
+								break;
+							case 2: // Down
+								break;
+							case 3: // Right
+								// Draw the door on the right
+								verticalDoor.setPosition(tmpX * BLOCK_SIZE, tmpY * BLOCK_SIZE);
+								target.draw(verticalDoor, states);
+
+								// Draw the door on the left
+								verticalDoor.setPosition(tmpX * BLOCK_SIZE - VDOOR_WIDTH, tmpY * BLOCK_SIZE);
+								target.draw(verticalDoor, states);
+								break;
+							default: // Left
+								break;
+						}
+						done = true;
+					}
+				}
+			}
 	}
 }
 
--- src/Map.h
+++ src/Map.h
@@ -7,6 +7,18 @@
 
 #include "Level.h"
 
+struct LevelConnection
+{
+	LevelConnection(Level *l1, Level *l2):
+		level1(l1),
+		level2(l2)
+	{
+	}
+
+	Level *level1;
+	Level *level2;
+};
+
 class Map : public sf::Drawable
 {
 	public:
@@ -17,9 +29,10 @@ class Map : public sf::Drawable
 
 		virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
 	private:
-		
-		// data
+		void drawDoors(sf::RenderTarget &target, sf::RenderStates states) const;
+
 		std::vector<Level*> levels;
+		std::vector<LevelConnection*> connections;
 };
 
 #endif
