8edec7c6dcfd035e830bf5ccc57981a0a88257b3
Pierre Godard <pierre.godard@insa-lyon.fr>
2015-02-08 19:28:45 +0100

    First remake of the physics

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -27,29 +27,94 @@ void Physics::CleanEntities()
 
 void Physics::manageStaticCollisions(Entity* e)
 {
-    if( !(e->pos.x < 0 || e->pos.x+e->GetSprite().getGlobalBounds().width > collisionMap->getSize().x
-       || e->pos.y < 0 || e->pos.y+e->GetSprite().getGlobalBounds().height > collisionMap->getSize().y) )
+    //// Forces
+    
+    //// Map bounds collisions
+    
+    
+    // Entity too on the left
+    if(e->pos.x < 0)
     {
-
-        for(unsigned int x=e->pos.x;
-            x<e->pos.x+e->GetSprite().getGlobalBounds().width;
-            ++x)
+        e->pos.x = 0;
+        e->forces.x = e->forces.x < 0 ? 0 : e->forces.x;
+        e->a.x = e->a.x < 0 ? 0 : e->a.x;
+        e->v.x = e->v.x < 0 ? 0 : e->v.x;
+    }
+    // Entity too on the right
+    else if(e->pos.x+e->GetSprite().getGlobalBounds().width > collisionMap->getSize().x)
+    {
+        e->pos.x = collisionMap->getSize().x - e->GetSprite().getGlobalBounds().width;
+        e->forces.x = e->forces.x > 0 ? 0 : e->forces.x;
+        e->a.x = e->a.x > 0 ? 0 : e->a.x;
+        e->v.x = e->v.x > 0 ? 0 : e->v.x;
+    }
+    
+    //// Structure collisions
+    
+    if (isThereStaticCollision(e))
+    {
+        int yCorrection = computeVerticalStaticCorrection(e);
+        if (yCorrection > 0)
         {
-            for(unsigned int y=e->pos.y;
-                    y<e->pos.y+e->GetSprite().getGlobalBounds().height;
-                    ++y)
+            e->forces.y = e->forces.y < 0 ? 0 : e->forces.y;
+            e->a.y = e->a.y < 0 ? 0 : e->a.y;
+            e->v.y = e->v.y < 0 ? 0 : e->v.y;
+        }
+        else if (yCorrection < 0)
+        {
+            e->forces.y = 0;
+            e->a.y = 0;
+            e->v.y = 0;
+        }
+        e->pos.y+=yCorrection;
+    }
+    else
+    {
+        e->forces = GRAVITY;
+    } 
+}
+
+bool Physics::isThereStaticCollision(Entity* e)
+{
+    for(unsigned int x=e->pos.x;
+        x<e->pos.x+e->GetSprite().getGlobalBounds().width;
+        ++x)
+    {
+        for(unsigned int y=e->pos.y;
+                y<e->pos.y+e->GetSprite().getGlobalBounds().height;
+                ++y)
+        {
+            if(collisionMap->getPixel(x,y).r > 0)
             {
-                if(collisionMap->getPixel(x,y).r > 0)
-                {
-                    e->forces = sf::Vector2f();
-                    e->a = sf::Vector2f();
-                    e->v = sf::Vector2f();
-                    return;
-                }
+                return true;
             }
         }
     }
-    e->forces = GRAVITY;
+    return false;
+}
+
+int Physics::computeVerticalStaticCorrection(Entity* e)
+{
+    unsigned int x = (2*e->pos.x+e->GetSprite().getGlobalBounds().width-1)/2;
+    unsigned int yBottom = e->pos.y+e->GetSprite().getGlobalBounds().height-1;
+    
+    // Test the map collision vertical bounds
+    if (e->pos.y < 0)
+    {
+        return -e->pos.y;
+    }
+    else if (e->pos.y+e->GetSprite().getGlobalBounds().height > collisionMap->getSize().y)
+    {
+        return collisionMap->getSize().y-1 - yBottom;
+    }
+    
+    // Search the highest collision point
+    int y=0; // y coordonate of the collision point
+    for(y=0;
+        y < e->GetSprite().getGlobalBounds().width
+            && collisionMap->getPixel(x,yBottom+y).r > 0;
+        --y);
+    return y;
 }
 
 void Physics::Update()
