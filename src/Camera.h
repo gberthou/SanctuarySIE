c095ac6bd12c77b2623e942177aa3928cee82a4f
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-02-11 16:06:10 +0100

    Adapted makefile

--- src/Camera.h
+++ src/Camera.h
@@ -3,8 +3,8 @@
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <Character.h>
-#include <Level.h>
+#include "Character.h"
+#include "Level.h"
 
 class Camera : public sf::View
 {
